80d012ca33e3964eae17e1b265b386d4f42a418b
YicongY <able1994@hotmail.com>
2018-04-16 20:20:42 -0500

    Addd

--- backend/app.py
+++ backend/app.py
@@ -6,4 +6,4 @@ def display():
     return render_template('example.html')
 
 if __name__=='__main__':
-    app.run(debug = True, host = '0.0.0.0',port=8888)
+    app.run(debug = True, host = '0.0.0.0',port=1234)
