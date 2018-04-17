e0e11fc7e5884792d5e7b638222cc280ace6f42b
YicongY <able1994@hotmail.com>
2018-04-17 16:22:10 -0500

    Add

--- ReMine/app.py
+++ ReMine/app.py
@@ -30,5 +30,5 @@ def runRemine():
 
 
 if __name__=='__main__':
-    #app.run(debug = True, host = '0.0.0.0',port=1111)
-    app.run(debug = True, host = 'localhost', port=5000)
+    app.run(debug = True, host = '0.0.0.0',port=1111)
+    #app.run(debug = True, host = 'localhost', port=5000)
