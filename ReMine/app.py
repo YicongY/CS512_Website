d8a0553c7b20fda8b6dc46d75e9327e458e28a93
Cheng Ding <july@wirelessprv-10-194-190-91.near.illinois.edu>
2018-04-17 16:08:43 -0500

    mm

--- ReMine/app.py
+++ ReMine/app.py
@@ -25,5 +25,5 @@ def runRemine():
 
 
 if __name__=='__main__':
-    app.run(debug = True, host = '0.0.0.0',port=1111)
-    #app.run(debug = True,host = '0.0.0.0')
+    #app.run(debug = True, host = '0.0.0.0',port=1111)
+    app.run(debug = True, host = 'localhost', port=5000)
--- ReMine/static/func.js
+++ ReMine/static/func.js
@@ -1,5 +1,9 @@
 function submitCorpus() {
     let c = document.getElementById('inText').value;
-    console.log(c);
-    document.getElementById("outText").innerHTML=c;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://dmserv4.cs.illinois.edu:1111/remine", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send();
+    console.log(xhr);
+    document.getElementById("outText").innerHTML=xhr.responseText;
 }
