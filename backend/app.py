c6c2dd48d8670a48c457ffcfc528c4dd05618f74
YicongY <able1994@hotmail.com>
2018-04-16 21:37:32 -0500

    add

--- backend/app.py
+++ backend/app.py
@@ -9,8 +9,9 @@ def render():
 
 @app.route('/C')
 def runC():
-    result = subprocess.run(['./../c++/q1'])
-    return result
+    result = subprocess.run(['./../c++/q1'],stdout = subprocess.PIPE)
+    return result.stdout
 
 if __name__=='__main__':
-    app.run(debug = True, host = '0.0.0.0',port=1111)
+    #app.run(debug = True, host = '0.0.0.0',port=1111)
+    app.run(debug = True,host = '0.0.0.0')
