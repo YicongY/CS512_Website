5010c60eb0f803f170a2a0989d8d64639740b779
Cheng Ding <july@wirelessprv-10-194-190-91.near.illinois.edu>
2018-04-17 16:18:37 -0500

    b

--- ReMine/static/func.js
+++ ReMine/static/func.js
@@ -7,3 +7,46 @@ function submitCorpus() {
     console.log(xhr);
     document.getElementById("outText").innerHTML=xhr.responseText;
 }
+
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+// Make the actual CORS request.
+function makeCorsRequest() {
+  // This is a sample server that supports CORS.
+  var url = 'http://dmserv4.cs.illinois.edu:1111/remine';
+
+  var xhr = createCORSRequest('POST', url);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+
+  // Response handlers.
+  xhr.onload = function() {
+    var text = xhr.responseText;
+    var title = getTitle(text);
+    alert('Response from CORS request to ' + url + ': ' + title);
+  };
+
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+
+  xhr.send();
+  console.log(xhr.responseText);
+}
