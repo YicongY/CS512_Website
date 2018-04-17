71c5630280401c58720bc6fba632f628c029d92c
YicongY <able1994@hotmail.com>
2018-04-17 13:15:56 -0500

    edd

--- c++/q1.cpp
+++ c++/q1.cpp
@@ -4,10 +4,10 @@ using namespace std;
 #include<string>
 int main(){
 
-   int your_age=22;
+   int your_age=1;
    
    string your_name;
-   your_name="Xiaolu Qi";
+   your_name="Remine Test";
   cout <<"Hello world! My name is "<<your_name <<" and I am "<<your_age<<" years old.\n";
   return 0;
 }
