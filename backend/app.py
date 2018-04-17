442922bc36b519a04e0485c222492215409772dc
YicongY <able1994@hotmail.com>
2018-04-16 20:40:34 -0500

    add

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,8 @@ from flask import Flask,request,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def display():
+def render():
     return render_template('example.html')
 
 if __name__=='__main__':
-    app.run(debug = True, host = '0.0.0.0',port=1233)
+    app.run(debug = True, host = '0.0.0.0',port=1111)
