1dc01add495a838f1d907ffa97bb03a77fceea80
YicongY <able1994@hotmail.com>
2018-04-16 21:26:12 -0500

    add

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,16 @@
 from flask import Flask,request,render_template
 
-
+import subprocess
 app = Flask(__name__)
 
 @app.route('/')
 def render():
     return render_template('example.html')
+
+@app.route('/C')
+def runC():
+    result = subprocess.run(['./../c++/q1'])
+    return result
 
 if __name__=='__main__':
     app.run(debug = True, host = '0.0.0.0',port=1111)
