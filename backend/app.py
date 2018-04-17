57199873f0eca46e1047308535f7664304fb066c
YicongY <able1994@hotmail.com>
2018-04-16 20:56:24 -0500

    add

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,5 @@
 from flask import Flask,request,render_template
-from OpenSSL import SSL
 
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file('server.key')
-context.use_certificate_file('server.crt')
 
 app = Flask(__name__)
 
@@ -12,4 +8,4 @@ def render():
     return render_template('example.html')
 
 if __name__=='__main__':
-    app.run(debug = True, host = '0.0.0.0',port=1111,ssl_context=context)
+    app.run(debug = True, host = '0.0.0.0',port=1111)
