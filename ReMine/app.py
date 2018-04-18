e3a7241ee52c09b3a72aa3fa2eb8a12b509b235c
YicongY <able1994@hotmail.com>
2018-04-17 23:05:10 -0500

    Add

--- ReMine/app.py
+++ ReMine/app.py
@@ -39,6 +39,6 @@ def runRemine():
 
 if __name__=='__main__':
     #app.run(debug = True, host = '0.0.0.0',port=1111)
-    http_server = wsgi.WSGIServer(('0.0.0.0', 1111), app)
+    http_server = WSGIServer(('0.0.0.0', 1111), app)
     http_server.serve_forever()
     #app.run(debug = True, host = 'localhost', port=5000)
