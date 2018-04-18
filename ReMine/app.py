40aa4292486151f090473362baef9d2efbd3a9bf
YicongY <able1994@hotmail.com>
2018-04-17 20:15:25 -0500

    Add

--- ReMine/app.py
+++ ReMine/app.py
@@ -1,6 +1,8 @@
 from flask import Flask,request,render_template,jsonify,Response
 import subprocess
 from subprocess import Popen,PIPE
+from gevent.wsgi import WSGIServer
+
 #from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 #cors = CORS(app)
@@ -33,5 +35,7 @@ def runRemine():
 
 
 if __name__=='__main__':
-    app.run(debug = True, host = '0.0.0.0',port=1111)
+    #app.run(debug = True, host = '0.0.0.0',port=1111)
+    http_server = WSGIServer(('0.0.0.0', 1111), app)
+    http_server.serve_forever()
     #app.run(debug = True, host = 'localhost', port=5000)
