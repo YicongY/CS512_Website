658ed39418af41fc5778bc0af536c47c82d80ca7
Cheng Ding <july@wirelessprv-10-194-190-91.near.illinois.edu>
2018-04-21 16:30:06 -0500

    heiheie

---
+++ ReMine/Sample.py
@@ -0,0 +1,21 @@
+output = []
+lines = sum(1 for line in open('../qiz3/data/bio_pubtator/bio_pubtator.tokens.txt'))
+import random
+pick = random.sample(range(0, lines), 3)
+with open("test.txt") as fp:
+    line = fp.readline()
+    index = 0
+    while line:
+        if index in pick:
+            output.append(line.strip())
+        line = fp.readline()
+        index += 1
+output = '\n'.join(output)
+
+text_file = open("/tmp/Sample.txt", "w")
+text_file.write(output)
+text_file.close()
+
+
+
+
