fff808172d273ccc1e2181480bd8e27f079a25ff
Cheng Ding <july@wirelessprv-10-194-190-91.near.illinois.edu>
2018-04-21 21:23:59 -0500

    heiheie

--- ReMine/Sample.py
+++ ReMine/Sample.py
@@ -1,6 +1,5 @@
-lines = sum(1 for line in open('../../../qiz3/data/bio_pubtator/bio_pubtator.tokens.txt'))
 import random
-pick = random.sample(range(0, lines), 100000)
+pick = random.sample(range(0, 3000000), 100000)
 
 output = []
 out_str = []
@@ -25,9 +24,8 @@ text_file.write(output)
 text_file.close()
 
 
-lines = sum(1 for line in open('../../../qiz3/data/wiki/sampled_wiki.tokens.txt'))
 import random
-pick = random.sample(range(0, lines), 100000)
+pick = random.sample(range(0, 3000000), 100000)
 
 output = []
 out_str = []
