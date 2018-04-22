13e0c30f81872f2577af9ebb710c77b910fb6bda
Cheng Ding <july@wirelessprv-10-194-190-91.near.illinois.edu>
2018-04-21 21:18:01 -0500

    heiheie

--- ReMine/Sample.py
+++ ReMine/Sample.py
@@ -1,30 +1,52 @@
-output = []
-out_str = []
 lines = sum(1 for line in open('../../../qiz3/data/bio_pubtator/bio_pubtator.tokens.txt'))
 import random
 pick = random.sample(range(0, lines), 100000)
+
+output = []
+out_str = []
 with open('../../../qiz3/data/bio_pubtator/bio_pubtator.tokens.txt') as fp:
     out_str = fp.read().split('\n')
 for i in pick:
     output.append(out_str[i])
 output = '\n'.join(output)
-
-text_file = open("tmp/Sample_bio_pubtator.txt", "w")
+text_file = open("tmp/Sample_bio_pubtator.token.txt", "w")
 text_file.write(output)
 text_file.close()
 
 output = []
 out_str = []
+with open('../../../qiz3/data/bio_pubtator/bio_pubtator.pos.txt') as fp:
+    out_str = fp.read().split('\n')
+for i in pick:
+    output.append(out_str[i])
+output = '\n'.join(output)
+text_file = open("tmp/Sample_bio_pubtator.pos.txt", "w")
+text_file.write(output)
+text_file.close()
+
+
 lines = sum(1 for line in open('../../../qiz3/data/wiki/sampled_wiki.tokens.txt'))
 import random
 pick = random.sample(range(0, lines), 100000)
+
+output = []
+out_str = []
 with open('../../../qiz3/data/wiki/sampled_wiki.tokens.txt') as fp:
     out_str = fp.read().split('\n')
 for i in pick:
     output.append(out_str[i])
 output = '\n'.join(output)
-
-text_file = open("tmp/Sample_wiki.txt", "w")
+text_file = open("tmp/Sample_wiki.token.txt", "w")
+text_file.write(output)
+text_file.close()
+output = []
+out_str = []
+with open('../../../qiz3/data/wiki/sampled_wiki.pos.txt') as fp:
+    out_str = fp.read().split('\n')
+for i in pick:
+    output.append(out_str[i])
+output = '\n'.join(output)
+text_file = open("tmp/Sample_wiki.pos.txt", "w")
 text_file.write(output)
 text_file.close()
 
