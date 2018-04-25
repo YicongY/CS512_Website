22a281600e737dc895dc8e28411d499c1bff32c4
YicongY <able1994@hotmail.com>
2018-04-25 14:44:22 -0500

    add

--- ReMine/server.py
+++ ReMine/server.py
@@ -91,6 +91,7 @@ if __name__=='__main__':
     session = server.find_where({"session_name": "preload"})
     window = session.new_window(attach=False, window_name="remine")
     pane = window.split_window(attach=False)
+    pane.send_keys('cd /shared/data/remine/CS512_Website/', enter = True)
     pane.send_keys('./bin/remine --model pre_train/segmentation.model --mode 1', enter=True)
 
     http_server = WSGIServer(('0.0.0.0', 1111), app)
